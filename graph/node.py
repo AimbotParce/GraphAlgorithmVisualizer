adf4543b95c133d166cb84b5c5722cb86f1452a7
AimbotParce <marcparcerisa@gmail.com>
2024-12-10 13:23:37 +0100

    Grant access to everything to logger

--- graph/node.py
+++ graph/node.py
@@ -3,7 +3,7 @@ import contextvars
 from collections import deque
 from io import IOBase
 from os import PathLike
-from typing import Callable, Generic, ParamSpec, TextIO, TypeVar, overload
+from typing import Any, Callable, Generic, ParamSpec, TextIO, TypeVar, overload
 
 T = TypeVar("T")
 P = ParamSpec("P")
@@ -14,12 +14,19 @@ class NodeVisitLogger:
     def __init__(self, fd: TextIO, fmt: str = "%(id)s") -> None: ...
     @overload
     def __init__(self, fd: PathLike, fmt: str = "%(id)s") -> None: ...
+    @overload
+    def __init__(self, fd: None, fmt: str = "%(id)s") -> None: ...
 
-    def __init__(self, fd: PathLike | TextIO, fmt: str = "%(id)s"):
-        if isinstance(fd, IOBase):
+    def __init__(self, fd: PathLike | TextIO | None = None, fmt: str = "%(id)s"):
+        if fd is None:
+            self._fd = None
+            self._log = list[str]()
+        elif isinstance(fd, IOBase):
             self._fd = fd
+            self._log = None
         else:
             self._fd = open(fd, "w")
+            self._log = None
         self._fmt = fmt
         self._opened = not isinstance(fd, IOBase)
         self._token: contextvars.Token = None
@@ -34,8 +41,15 @@ class NodeVisitLogger:
             self._fd.close()
 
     def logVisit(self, node: "BaseNode") -> None:
-        self._fd.write(self._fmt % {"id": node._id, "content": node._content, "visited": node._visited})
-        self._fd.write("\n")
+        cont: dict[str, Any] = {}
+        for k in node.__dict__:
+            cont[k.lstrip("_")] = getattr(node, k)
+        line = self._fmt % cont
+        if self._log is not None:
+            self._log.append(line)
+        else:
+            self._fd.write(line)
+            self._fd.write("\n")
 
     @staticmethod
     def on_call(callable: Callable[P, T]) -> Callable[P, T]:
